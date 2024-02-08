edad84bd2badac7c7b93fccbf627023c0a60f9c4
Nikolay Proschalykin <n.proschalykin@mail.ru>
2024-02-08 15:28:41 +0300

    setup is done
    Добавлено и настроено:
    - cocoaPods
    - SwiftLint
    
    Удалено:
    - storyBoard

---
+++ CountDown/CountDown/Features/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  CountDown
+//
+//  Created by Николай Прощалыкин on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+}
---
+++ CountDown/Pods/Target Support Files/Pods-CountDown-CountDownUITests/Pods-CountDown-CountDownUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CountDown_CountDownUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CountDown_CountDownUITests
+@end
